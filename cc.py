a51a48a616467ce4fc0765705966d5be5d21ecdd
Marcelo Rodriguez <marcelo@rodriguez.com.mx>
2016-04-30 13:16:27 -0500

    decouple diff's and archive options. base_diff now working Changed format of the archive option , remove 'running' from the string.

--- cc.py
+++ cc.py
@@ -63,9 +63,11 @@ class SteelHeadCC(Application):
         super(SteelHeadCC, self).add_options(parser)
         parser.add_option('-u', '--username', help="SteelHead username, default=admin", default="admin")
         parser.add_option('-p', '--password', help="Password for all SteelHead's")
-        parser.add_option('-a', '--archive', action="store_true",dest="archive", default=True, help="archive configuration file")
-        parser.add_option('-d', '--diff', action="store_true", dest="diff", default=False, help="diff against previous version")
-        parser.add_option('-b', '--base_diff', action="store_true", dest="base_diff", default=False,
+        parser.add_option('-a', '--archive', action="store_true",dest="archive", default=True,
+                          help="archive configuration file")
+        parser.add_option('-d', '--diff', action="store_true", dest="diff", default=False,
+                          help="diff against previous version")
+        parser.add_option('-b', '--base_diff', dest="base_filename",
                           help="diff all steelheads against a common base configuration file")
         parser.add_option('', '--html', action="store_true", dest="html", default=False, help="output html file")
         parser.add_option('-t', '--threads', help="how many concurrent devices to process at a given time",
@@ -103,7 +105,7 @@ class SteelHeadCC(Application):
                 pass
 
             try:
-                self.options.base_diff = config.get('Main', 'base_diff')
+                self.options.base_filename = config.get('Main', 'base_diff')
             except (ConfigParser.NoSectionError, ConfigParser.NoOptionError):
                 pass
 
@@ -127,8 +129,13 @@ class SteelHeadCC(Application):
         if not self.options.password:
             self.options.password = getpass.getpass("Password for all SteelHead's:")
 
-        if self.options.html and not (self.options.diff or self.options.base_diff):
-            print('Cannot specifity html without dif or base_diff options')
+        if self.options.html and not (self.options.diff or self.options.base_filename):
+            print('Cannot specify html without dif or base_diff options')
+            exit(1)
+
+        if self.options.diff and self.options.base_filename:
+            print('Cannot specify diff and base_diff at the same time. Choose one or the other')
+            exit(1)
 
 
 
@@ -209,48 +216,68 @@ class SteelHeadCC(Application):
         :param latest_file: the latest filename found ( if found ) or None
         :return: tuple (previous_last_filename, latest_filename)
         """
-        new_latest = './logs/' + device_ip + '-running-' + date_string + '-latest.log'
+        new_latest = './logs/' + device_ip + '-' + date_string + '-latest.log'
         last_latest = None
         if latest_file:
             temp = latest_file
             last_latest = re.sub(r"-latest", "", latest_file)
-            new_latest = './logs/' + device_ip + '-running-' + date_string + '-latest.log'
+            new_latest = './logs/' + device_ip + '-' + date_string + '-latest.log'
             os.rename(temp,last_latest )
 
         file = open(new_latest, "w")
         file.write(running_config)
         file.close()
 
+
         return (last_latest, new_latest)
 
-    def diff_configs(self,logger=None, previous_filename=None, latest_filename=None, device_ip=None):
+    def diff(self, logger=None, type=None, previous_filename=None, running=None, device_ip=None):
+        '''
+        diff_to_previous , diff against previous configurations
+        :param logger: to log any errors
+        :param type: Type of diff either 'base' or 'running'
+        :param previous_filename: file name of a previous file to diff against
+        :param running: latest running configuration , String with the configuration ,will be splited in an array
+        :param device_ip: the device ip address
+        :return: None always ...
+        '''
         if previous_filename is None:
             logger.critical('Cannot find a previous config to diff against.')
             return None
         else:
-            logger.info('diff ' + previous_filename + ' vs. ' + latest_filename)
-
-            if os.path.exists(previous_filename) and os.path.exists(latest_filename):
+            if type is 'running':
+                logger.info('diff ' + previous_filename + ' vs. running')
+            elif type is 'base':
+                logger.info('diff ' + previous_filename + ' vs. base')
+            if os.path.exists(previous_filename):
                 previous_text = open(previous_filename, "U").readlines()
-                latest_text = open(latest_filename, "U").readlines()
                 previous_date_match = re.search(r'\d{8}-\d{6}', previous_filename)
-                latest_date_match = re.search(r'\d{8}-\d{6}', latest_filename)
-
+                running_text = running.split('\n')
                 if self.options.html:
-                    diff = difflib.HtmlDiff().make_file(previous_text,latest_text,previous_filename,latest_filename,
-                                                        True)
+                    if type is 'running':
+                        diff = difflib.HtmlDiff().make_file(previous_text,running_text,previous_filename,'running',
+                                                            True)
+                    elif type is 'base':
+                        diff = difflib.HtmlDiff().make_file(previous_text, running_text, previous_filename, 'running',
+                                                            True)
                     try:
-                        diff_file = open('./logs/' + device_ip + '-' + previous_date_match.group() + '-' + latest_date_match.group() + '.diff.html', "w+")
+                        if type is 'running':
+                            diff_file = open(
+                                './logs/' + device_ip + '-' + previous_date_match.group() + '-running.diff.html', "w+")
+                        elif type is 'base':
+                            diff_file = open(
+                                './logs/' + device_ip + '-' + previous_date_match.group() + '-running.diff.html', "w+")
                         diff_file.write(diff)
                         diff_file.close()
                     except AttributeError as e:
                         logger.info('cannot find date in file string')
-                        return
+                        return None
                 else:
-                    diff = difflib.context_diff(previous_text, latest_text, previous_filename,latest_filename,
-                                                previous_date_match.group(), latest_date_match.group())
+                    diff = difflib.context_diff(previous_text, running_text, previous_filename,'running',
+                                                previous_date_match.group(), 'running')
 
-                    diff_file = open('./logs/' + device_ip + '-' + previous_date_match.group() + '-' + latest_date_match.group() + '.diff.txt', "w+")
+                    diff_file = open(
+                        './logs/' + device_ip + '-' + previous_date_match.group() + '-running.diff.txt', "w+")
                     try:
                         for line in diff:
                             diff_file.write(line)
@@ -258,7 +285,7 @@ class SteelHeadCC(Application):
                         diff_file.close()
                     except AttributeError as e:
                         logger.info('cannot find date in file string')
-                        return
+                        return None
 
             else:
                 logger.error('Cannot read files to diff')
@@ -285,11 +312,21 @@ class SteelHeadCC(Application):
         latest_filename = self.get_latest_file(device_ip)
         diff_files = None
 
-        if self.options.archive:
-            diff_files = self.archive_running_config(device_ip, sh_running, date_string, latest_filename)
+        latest_filename = self.get_latest_file(device_ip)
+        if latest_filename:
+            if self.options.diff:
+                self.diff(logger, 'running',latest_filename, sh_running, device_ip)
+            elif self.options.base_filename:
+                if os.path.exists(self.options.base_filename):
+                    base_fd = open(self.options.base_filename,"r")
+                    base_text = base_fd.read()
+                    self.diff(logger, 'base',latest_filename, base_text, device_ip)
+                else:
+                    logger.critical('Base config file not found exiting')
+                    exit(1)
+            if self.options.archive:
+                 self.archive_running_config(device_ip, sh_running, date_string, latest_filename)
 
-        if self.options.diff and diff_files:
-            self.diff_configs(logger, *diff_files, device_ip=device_ip)
 
         time.sleep(5)
 
